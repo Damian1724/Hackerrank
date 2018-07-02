d095a1f5f0b8e120b081ed6c4ea9cee560c805e8
Damian1724 <39019058+Damian1724@users.noreply.github.com>
2018-07-01 23:26:39 -0400

    Update Tree:_Postorder_Traversal.py

--- data-structure/Trees/Tree:_Postorder_Traversal.py
+++ data-structure/Trees/Tree:_Postorder_Traversal.py
@@ -12,14 +12,12 @@ self.left (the left child of the node)
 self.right (the right child of the node)
 self.info (the value of the node)
 """
-def visiting_tree(cur_root,lista):
+def visiting_tree(cur_root):
     if cur_root is not None:
-        lista.append(cur_root)
-        visiting_tree(cur_root.left,lista)
-        visiting_tree(cur_root.right,lista)
+        visiting_tree(cur_root.left)
+        visiting_tree(cur_root.right)
         print(cur_root, end=" ")
         
 def postOrder(root):
-    lista=[]
     if root is not None:
-        visiting_tree(root,lista)
+        visiting_tree(root)
